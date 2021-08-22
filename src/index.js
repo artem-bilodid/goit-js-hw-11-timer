7d62e3650dc358f5f46eb12abda8959ea3c8b5da
Artem Bilodid <ar.bilodid@draftkings.com>
2021-08-22 14:30:44 +0300

    changed import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/main.scss';
-import CountdownTimer from './js/CountdownTimer';
+import CountdownTimer from './js/CountdownTimer.js';
 
 const timer = new CountdownTimer('#timer-1', new Date('Jan 23, 2022'));
 timer.start();
